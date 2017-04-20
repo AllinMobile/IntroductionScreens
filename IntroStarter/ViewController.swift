0320b6716b253d71f15551e3858229acb920b5ca
Ada Chmielewska <adachmielewska@MacBook-Pro-Ada.local>
2017-04-20 08:12:25 +0200

    starter

---
+++ IntroStarter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IntroStarter
+//
+//  Created by Ada Chmielewska on 16.02.2017.
+//  Copyright © 2017 All in mobile. All rights reserved.
+//
+
+import UIKit
+import Presentation
+
+class ViewController: PresentationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
